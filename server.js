206ae57d373c0db9e6e8ae46d46130f7a787bea2
Piyush <02jan06@gmail.com>
2024-10-03 15:38:26 +0530

    Added dotenv

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors")
+require('dotenv').config()
 app.use(cors({
   origin: '*'
 }));
-
+const PORT = process.env.PORT;
 app.get("/", async (req, res) => {
   const datascrape = await scrapeArxiv();
   res.json(datascrape);
@@ -15,6 +16,6 @@ app.get("/", async (req, res) => {
 app.use((req, res, next, err) => {
   res.status(404).send("404 not found");
 });
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
